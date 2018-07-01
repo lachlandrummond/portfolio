2e8e93bb6bc105d2d5668ecfc638781fdc4ae42d
lachlandrummond <lachlan.drummond.work@gmail.com>
2018-07-01 20:30:41 +1200

    base

---
+++ js/render.js
@@ -0,0 +1,27 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.getElementById("render").appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	function logMouseMove(e) {
+	e = event || window.event;
+	mousePos = { x: e.clientX, y: e.clientY };
+	console.log(mousePos);
+	}
+
+	logMouseMove();
+
+	renderer.render( scene, camera );
+}
+animate();
