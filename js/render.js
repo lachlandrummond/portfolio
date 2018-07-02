e8e1faf1bc0c843c3c13ce61938914186e34ba6d
lachlandrummond <lachlan.drummond.work@gmail.com>
2018-07-03 10:49:50 +1200

    floor + prop

--- js/render.js
+++ js/render.js
@@ -1,43 +1,42 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.getElementById("render").appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+var cubeg = new THREE.BoxGeometry( 50, 200, 1 );
+var cubem = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( cubeg, cubem );
+cube.position.z = -500;
 
-var g = new THREE.SphereGeometry( 0.5, 32, 32 );
-var m = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-var sphere = new THREE.Mesh( g, m );
-sphere.position.x = -2;
+var roofg = new THREE.PlaneGeometry(1000, 1000, 1);
+var roofm = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var roof = new THREE.Mesh( roofg, roofm );
+roof.rotation.x = -90;
 
-var g2 = new THREE.CylinderGeometry( 0.5, 0.5, 1, 32 );
-var m2 = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
-var triangle = new THREE.Mesh( g2, m2 );
-triangle.position.x = 2;
+scene.add( cube, roof );
 
-scene.add( cube, sphere, triangle );
+camera.rotation.x = -0.2;
 
-camera.position.z = 5;
+document.addEventListener("mousemove", function(event){
+	var x = event.clientX;
+	var y = event.clientY;
+	var cx = x - window.innerWidth / 2;
+	var cy = y - window.innerHeight / 2;
+	camera.position.x = -cx / 10;
+	camera.position.y = cy / 10;
+	camera.position.y = camera.position.y + 100;
+})
 
 function animate() {
 	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+
 	window.addEventListener('resize', onWindowResize, false);
 	function onWindowResize(){
 		camera.aspect = window.innerWidth / window.innerHeight;
     	camera.updateProjectionMatrix();
     	renderer.setSize( window.innerWidth, window.innerHeight );
 	}
-	document.addEventListener("mousemove", function(event){
-		var x = event.clientX;
-		var y = event.clientY;
-		var cx = x - window.innerWidth / 2;
-		var cy = y - window.innerHeight / 2;
-		camera.position.x = cx / 500;
-		camera.position.y = -cy / 500;
-	})
+	renderer.render( scene, camera );
 }
 animate();
