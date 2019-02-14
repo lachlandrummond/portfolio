23f6429a375a30c8fc642447ca74ae08157e7bbf
lachlandrummond <lachlan.drummond.work@gmail.com>
2019-02-14 17:19:11 +1300

    implementing menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var content = document.getElementById("content-container");
+var nav = document.getElementById("nav");
+var hamTop = document.getElementById("ham-top");
 
 function navToggle(){
 	if(content.style.marginLeft == "0px"){
-		TweenMax.to("#content-container", 0.6, {marginLeft: "500px", ease: Expo.easeInOut});
+		TweenMax.to("#content-container", 0.6, {marginLeft: "300px", ease: Expo.easeInOut});
 	} else {
 		TweenMax.to("#content-container", 0.6, {marginLeft: "0px", ease: Expo.easeInOut});
 	}
